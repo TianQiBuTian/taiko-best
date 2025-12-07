a6170c9d1dfda459647b3d926bd12be54ac2b097
KirisameVanilla <118162831+kirisamevanilla@users.noreply.github.com>
2025-12-08 04:29:57 +0800

    docs: fix guide

--- src/components/GuideView.vue
+++ src/components/GuideView.vue
@@ -42,19 +42,20 @@ const handleAnalyze = () => {
 
 <template>
   <div class="container">
-    <h6>更新时间:2025/12/05</h6>
+    <h6>更新时间: 2025/12/05</h6>
     <h6>算法仍在调整中</h6>
-    <h6>本Rating系统旨在分析自身弱点并针对练习,请勿用于攀比</h6>
-    <h2>使用指南</h2>
-    <p>1. 启动传分器,按照指引打开电脑端广场爬分,直到传分器走到在DonNote点击上传按钮之前的一步(不需要打开donnote,更不需要点击上传按钮)</p>
-    <p>2. 将浏览器代理设置到系统代理,打开<a href="https://www.baidu.com/api/ahfsdafbaqwerhue" target="_blank">baidu</a>,传分器会将分数传到页面中,ctrl+a全选复制过来粘贴</p>
-    <p>3. 如果不会设置浏览器代理,按win键搜索PowerShell,将以下代码粘贴并回车执行,会把数据复制到你的剪贴板。<a href="javascript:void(0);" @click="copyPowerShellCode">复制代码</a></p>
+    <h6>本 Rating 系统旨在分析自身弱点并针对练习, 请勿用于攀比</h6>
+    <h2>使用指南(需要使用电脑)</h2>
+    <p>1. 启动传分器, 按照指引打开电脑端广场爬分, 直到传分器走到在 DonNote 点击上传按钮之前的一步(不需要打开 DonNote, 更不需要点击上传按钮)</p>
+    <p>2. 将浏览器代理设置到系统代理,打开 <a href="https://www.baidu.com/api/ahfsdafbaqwerhue" target="_blank">获取成绩</a>, 传分器会将分数传到页面中, ctrl + a 全选复制过来粘贴</p>
+    <p>3. 如果不会设置浏览器代理, 按 win 键搜索 PowerShell, 将以下代码粘贴并回车执行 <a href="javascript:void(0);" @click="copyPowerShellCode">点我复制代码</a></p>
     <div class="links-section">
       <p class="links-title">传分器链接:</p>
       <ul class="links-list">
-        <li><a href="https://gitee.com/donnote/taiko-score-getter/releases/tag/latest" target="_blank">旧版 donnote/taiko-score-getter Gitee</a></li>
-        <li><a href="https://github.com/Steve-xmh/taiko-score-getter-rs/releases/tag/v0.1.2" target="_blank">新版 Steve-xmh/taiko-score-getter-rs GitHub</a></li>
-        <li><a href="https://github.com/Steve-xmh/taiko-score-getter-rs/releases/latest/download/taiko-score-getter.exe" target="_blank">下载最新版传分器</a></li>
+        <li><a href="https://gitee.com/donnote/taiko-score-getter/releases/tag/latest" target="_blank">旧版@Gitee donnote/taiko-score-getter</a></li>
+        <li><a href="https://github.com/Steve-xmh/taiko-score-getter-rs/releases/tag/v0.1.2" target="_blank">新版@GitHub Steve-xmh/taiko-score-getter-rs</a></li>
+        <li><a href="https://github.com/Steve-xmh/taiko-score-getter-rs/releases/latest/download/taiko-score-getter.exe" target="_blank">点我下载新版传分器</a></li>
+        <li><a href="https://ghproxy.vanillaaaajp.workers.dev/https://github.com/Steve-xmh/taiko-score-getter-rs/releases/latest/download/taiko-score-getter.exe" target="_blank">点我使用代理下载新版传分器，大部分时间不用翻墙</a></li>
       </ul>
     </div>
     <div class="textarea-container">
