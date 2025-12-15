9589ecb3c5204e569ee6ce7289f02b79bcbed197
KirisameVanilla <118162831+kirisamevanilla@users.noreply.github.com>
2025-12-16 01:31:46 +0800

    feat: anouncement bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import NavigationBar from './components/NavigationBar.vue'
+import AnnouncementBar from './components/AnnouncementBar.vue'
 import Modal from './components/Modal.vue'
 import FloatingMenu from './components/FloatingMenu.vue'
 import { useModal } from './composables/useModal'
@@ -10,7 +11,8 @@ const { modalState, hideModal } = useModal()
 <template>
   <div id="app" class="flex flex-col min-h-screen">
     <NavigationBar />
-    <main class="flex-1 p-5">
+    <AnnouncementBar />
+    <main class="flex-1 p-5 pt-[60px] max-md:pt-[50px]">
       <router-view />
     </main>
     <FloatingMenu />
